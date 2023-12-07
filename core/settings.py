fd7eb404a1a155ee08689a54facd4e5d3b637534
El3auti <elbautisocial@gmail.com>
2023-12-06 22:41:18 -0300

    1

--- core/settings.py
+++ core/settings.py
@@ -18,6 +18,19 @@ ALLOWED_HOSTS = [
     "127.0.0.1"
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
 
 # Application definition
 
